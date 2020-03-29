86cd2a16af9dfa7c4210d1a375dd785ec8417a7c
kvbhushan2k <kvbhushan2k@gmail.com>
2020-03-29 14:42:26 +0100

    new project

---
+++ src/main/java/projectPkg/App.java
@@ -0,0 +1,13 @@
+package projectPkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! - Vidhya" );
+    }
+}
